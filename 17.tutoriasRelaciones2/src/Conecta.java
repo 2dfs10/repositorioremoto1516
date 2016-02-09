da8df7c7bd4b7b2366ab8e98388d95191a584c5d
2dfs10 <2dfs1011@gmail.com>
2016-02-09 22:33:13 +0100

    Update Conecta.java

--- 17.tutoriasRelaciones2/src/Conecta.java
+++ 17.tutoriasRelaciones2/src/Conecta.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-//LINEA
+//LINEA 222
 
 public class Conecta {
 	private final String controlador = "com.mysql.jdbc.Driver";
